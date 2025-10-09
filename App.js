d6692017aabde99a25d3352e2bc54bc87a4f8980
Sai Kiran <saikiran@aerosimple.in>
2025-10-09 17:35:44 +0530

    Launching of App

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// React.createElement => ReactElement-JS Object => HTMLElement(render)
 
-const heading = React.createElement(
-    "div",
-    {id:'parent'},
-    [React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1",{},"Hi H1 Tag!"),React.createElement("h2",{},"Hi H2 Tag!")]),
-    React.createElement(
-        "div",
-        {id:'child2'},
-        [React.createElement('h1',{},'Hi h1 Tag!'),React.createElement("h2",{},"Hi h2 Tag!")]
-    )
-    ]
+const heading = React.createElement("div",{id:'container',key:'container',},[
+  React.createElement('h1',{id:"heading",key:'heading'}, 'Namaste React'),
+  React.createElement('div',{id:'subContainer',key:'subContainer'},[
+    React.createElement('h1',{id:'mainHeading',key:'mainHeading',style:{color:'pink'}},"Main Heading"),
+    React.createElement('h2',{id:'subHeading',key:'subHeading',style:{color:'orange'}},"Sub Heading")
+  ])
+])
+
+// JSX - (transpiled before it reaches the JS Engine) => PARCEL - Babel
+// JSX - HTML like or XML Structured syntax code
+
+// JSX code => React.createElement => ReactElement-JS Object => HTMLElement 
+const mount = <h1 id="heading" className="container">Namaste React using JSX</h1>
+
+// React Element
+// React Element is a JSX element.
+
+// React Components are of Two Types
+// Class Based Components - Old
+// Functional Components - New
+
+// React Functional Components
+// Is a functional Components which return the piece of JSX code.
+// ComponentComposition
+const HeadingComponent = () => (
+  <>
+  {ele}
+  {Title()}
+  <Title/>
+  <SubHeading/>
+  <h1>Namaste React Functional Component</h1>
+  </>
 )
 
-    const root = ReactDOM.createRoot(document.getElementById('root'))
+const ele = (
+  <h1>Namaste React Element</h1>
+)
 
-    root.render(heading)
+const Title = () => (
+  <h1>Namaste React using JSX</h1>
+)
+
+const SubHeading =() => <h1>Namaste Heading Component</h1>
+
+console.log("mount >>>",mount)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<HeadingComponent/>)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React  
+
+
+# Parcel
+Dev Build
+Local Server
+HMR = Hot Module Replacement
+Parcel - It is doing it by File Watching Algorithm - written in c++
+Caching - Faster Builds
+Image Optimization
+Minification
+Bundling
+Compress javascript File
+Consistent Hashing
+Code Splitting
+Differential Bundling - support older browsers
+Diagnostic
+Error Handling
+Tree Shaking - remove unused code
+Different dev and prod bundles
